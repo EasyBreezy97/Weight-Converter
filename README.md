d6bc791d53ce939e21575d96c7fa84c10365080e
Guram <gurikakhasia@gmail.com>
2018-06-02 20:27:11 +0400

    added README

---
+++ README.md
@@ -0,0 +1,11 @@
+## Weight converter
+
+this is a good example for those who have just begin to learn 
+
+JavaScript. there is also Bootstrap classes and little css, HTML as well.
+
+you can convert with this app pounds into kilograms , grams and ounce, 
+
+kilograms into pounds, grams and ounce, ounce into kilograms, grams and pounds
+
+inspired by **Traversy Media**
--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 	window.onload=function(){
   		
-  		document.getElementById('output').style.visibility='hidden';
+  	//	document.getElementById('output').style.visibility='hidden';
 	 	
 	 	document.getElementById('lbsInput').addEventListener ('input',function (e){
 
-	 		document.getElementById('output').style.visibility='visible';
+	 		//document.getElementById('output').style.visibility='visible';
 			
 			let lbs = e.target.value; //rasac chavwert inputshi imis mnishvnelobas amoigebs
 			
-			document.getElementById('gramsOutput').innerHTML=lbs/0.0022046;
+			document.getElementById('gramsOutput').innerHTML=(lbs/0.0022046).toFixed(3);
 			
-			document.getElementById('kgOutput').innerHTML=lbs/2.20462262;
+			document.getElementById('kgOutput').innerHTML=(lbs/2.20462262).toFixed(3);
 			
-			document.getElementById('ozOutput').innerHTML=lbs*16;
+			document.getElementById('ozOutput').innerHTML=(lbs*16).toFixed(3);
 		});
 
+	 			document.getElementById('KgInput').addEventListener ('input',function(m){
+	 			
+	 			let kg=m.target.value;
+	 			
+	 				document.getElementById('lbOutput').innerHTML=(kg*2.20462262).toFixed(3);
+	 				document.getElementById('grOutput').innerHTML=(kg*1000).toFixed(3);
+	 				document.getElementById('ouncesOutput').innerHTML=kg*35.2739619.toFixed(3);
+
+	 	})
+
+	 	document.getElementById('OzInput').addEventListener('input',function(o){
+
+	 		let oz= o.target.value;
+	 		
+
+	 		document.getElementById('ozTokgOutput').innerHTML=oz*0.0283495231.toFixed(3);
+	 		document.getElementById('ozToGrams').innerHTML=oz*28.3495231.toFixed(3);
+	 		document.getElementById('ozToPounds').innerHTML=oz*0.0625.toFixed(3);
+
+	 	})		
+
 }
+
+			
+
